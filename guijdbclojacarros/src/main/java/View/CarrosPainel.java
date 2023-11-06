6cef674d9c94f77ede67dde3b9a4979360a91d05
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-11-06 15:25:32 -0300

    Carros

--- guijdbclojacarros/src/main/java/View/CarrosPainel.java
+++ guijdbclojacarros/src/main/java/View/CarrosPainel.java
@@ -1,14 +1,52 @@
 package View;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class CarrosPainel extends JPanel{
     // atributos - componentes
+    // campo de texto - JTextField
+    private JTextField inputPlaca;
+    private JTextField inputMarca;
+    private JTextField inputModelo;
+    private JTextField inputAno;
+    private JTextField inputCor;
+    private JTextField inputValor;
+    //campo escrito - JLabel
+    private JLabel labelPlaca;
+    private JLabel labelMarca;
+    private JLabel labelModelo;
+    private JLabel labelAno;
+    private JLabel labelCor;
+    private JLabel labelValor;
+    //tabela
+    private DefaultTableModel tableModel; //lógica
+    private JTable table; //visual
 
 
     // construtor(GUI-JPanel)
     public CarrosPainel() {
-        super();
+        //construir a tabela
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Placa");
+        tableModel.addColumn("Marca");
+        tableModel.addColumn("Modelo");
+        tableModel.addColumn("Ano");
+        tableModel.addColumn("Cor");
+        tableModel.addColumn("Valor");
+        table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+
+
+
+
+
+
         // entrada de dados
         // botões de eventos
         // tabela de carros
--- guijdbclojacarros/src/main/java/app/Main.java
+++ guijdbclojacarros/src/main/java/app/Main.java
@@ -1,7 +1,9 @@
 package app;
 
+import View.FramePrincipal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new FramePrincipal().run();
     }
 }
