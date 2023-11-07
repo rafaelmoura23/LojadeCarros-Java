b26a2ea0ff19c0e06f2898778ef606bd6747b6f1
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-11-07 15:31:07 -0300

    connection

--- guijdbclojacarros/src/main/java/Connection/ConnectionFactory.java
+++ guijdbclojacarros/src/main/java/Connection/ConnectionFactory.java
@@ -1,5 +1,42 @@
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";// nome do ADM do banco
+    private static final String SENHA = "postgres";// senha do ADM do banco
+
+    // métodos
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        try {
+            if (connection != null && stmt != null) {
+                stmt.close();
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
